a366ee2a65bc6cbcfb1b12542a363b828cf2cba7
devdeltek <devdeltek@gmail.com>
2023-04-17 17:53:18 -0700

    Finished hw4

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
@@ -20,14 +20,13 @@ public class GradebookServiceREST extends GradebookService {
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		
-		EnrollmentDTO enrollment = new EnrollmentDTO();
-		enrollment.course_id = course_id;
-		enrollment.studentEmail = student_email;
-		enrollment.studentName = student_name;
-		
-		System.out.println("Post to gradebook " + enrollment);
-		EnrollmentDTO response = restTemplate.postForObject(gradebook_url + "/enrollemt", enrollment, EnrollmentDTO.class);
-		System.out.println("Response from gradebook " + response);
+		 EnrollmentDTO enrollment = new EnrollmentDTO();
+		 enrollment.course_id = course_id;
+		 enrollment.studentEmail = student_email;
+		 enrollment.studentName = student_name;
+		 System.out.println("Post ot gradebook " + enrollment);
+		 EnrollmentDTO response = restTemplate.postForObject(gradebook_url+"/enrollment",enrollment, EnrollmentDTO.class);
+		 System.out.println("Response from gradebook " + response);
 	}
 
 }
